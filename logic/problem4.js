033889dcef738e8f25b535e411bc59e582162401
unknown <poojaharsule88@gmail.com>
2022-10-16 23:41:10 +0530

    logic

---
+++ logic/problem4.js
@@ -0,0 +1,15 @@
+//Given 3 numbers (all different values), print which is greatest
+
+let a = 8;
+let b = 2;
+let c = 4;
+
+if (a>b && a>c){
+  console.log("a is greater");
+}
+else if(b>a && b>c){
+  console.log("b is greater");
+}
+else{
+  console.log("c is greater");
+}
---
+++ logic/problem5.js
@@ -0,0 +1,27 @@
+// Given the days of the week in short format "Sun", "Mon" ... print in long format "Sunday", "Monday"
+
+let week_days= "fri";
+
+switch(week_days){
+
+    case "mon":console.log("monday");
+     break;
+
+    case "tue":console.log("tuesday");
+     break;
+    
+    case "wed":console.log("wednesday");
+     break; 
+
+    case "thur":console.log("thursday");
+     break; 
+
+    case "fri":console.log("friday");
+     break; 
+
+    case "sat":console.log("saturday");
+     break; 
+
+    case "sun":console.log("sunday");
+     break; 
+}
